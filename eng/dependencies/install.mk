aa0c1c4acb2f98cd0a626f314523f3786854f9af
smdn <smdn@smdn.jp>
2022-11-21 21:48:53 +0900

    add modification for CI builds on GitHub actions

---
+++ eng/dependencies/install.mk
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+# SPDX-License-Identifier: MIT
+#
+# This script installs the dependent apt packages to cross-compile the
+# Highlight shared library on GitHub Actions' runner image.
+#
+# For a list of dependencies, see [../src/README.md].
+#
+# SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+# SPDX-License-Identifier: MIT
+ubuntu.22.04.stamp:
+	sudo apt-get install -y \
+	  autotools-dev \
+	  libboost-dev \
+	  liblua5.3 \
+	  liblua5.3-dev \
+	  g++-mingw-w64-x86-64 \
+	  g++-multilib
+
+	touch $@
+
+install-ubuntu.22.04: ubuntu.22.04.stamp
+
+.PHONY: install-ubuntu22.04
