c2f44487b0dd766fd59554e7d5e63f86ea8a7ce4
smdn <smdn@smdn.jp>
2022-12-04 12:19:53 +0900

    add runner script for the workflow build-artifact.yml

---
+++ eng/build-artifact-for-all-targets.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+# SPDX-License-Identifier: MIT
+#
+# Summary:
+#   This script runs workflow that builds the artifacts (libhighlight,
+#   under the directory 'Smdn.LibHighlightSharp.Bindings/runtimes/**') on
+#   all build environment.
+#
+#   The built artifacts will be commited to individual branches and
+#   the workflow creates a new Pull Request which requests for merging
+#   that branch.
+#
+#   The branch to merge into is the branch where the workflow was started.
+#
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+read -p "Are you sure to start workflow on the branch '${BRANCH}'? (y/N) " yn
+
+case $yn in
+  [Yy]* )
+    break
+    ;;
+  * )
+    echo 'cancelled'
+    exit 1
+    ;;
+esac
+
+gh workflow run build-artifact.yml -f 'os=ubuntu-22.04, ubuntu-20.04, macos-11' -f 'verbose=true' --ref ${BRANCH}
