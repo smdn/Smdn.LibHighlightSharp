e14456b45db519669273d4dcd1bbd18bb3db4c97
smdn <smdn@smdn.jp>
2022-12-13 21:12:33 +0900

    add an example describes that the exception caused by the native library call order

---
+++ examples/nativeexception-loadlanguage-inittheme/Program.cs
@@ -0,0 +1,29 @@
+﻿// With Highlight version 4.x series, if the CodeGenerator::initTheme() is not called before
+// calling CodeGenerator::loadLanguage(), an unmanaged exception described below will be thrown
+// on calling CodeGenerator::generate*() and crash.
+//
+// terminate called after throwing an instance of 'std::out_of_range'
+//    what():  vector::_M_range_check: __n (which is 13) >= this->size() (which is 13)
+//
+
+using System;
+using System.IO;
+using Smdn.LibHighlightSharp.Bindings;
+
+var pathToDataDir = Path.Combine(
+  Path.GetDirectoryName(Environment.ProcessPath)!,
+  "highlight"
+);
+
+var pathToLangDefFile = Path.Join(pathToDataDir, "langDefs", "csharp.lang");
+var pathToThemeFile = Path.Join(pathToDataDir, "themes", "github.theme");
+
+using var generator = CodeGenerator.getInstance(OutputType.HTML);
+
+// Call CodeGenerator::loadLanguage before calling CodeGenerator::initTheme
+generator.loadLanguage(pathToLangDefFile);
+
+generator.initTheme(pathToThemeFile);
+
+// This will throw std::out_of_range
+Console.WriteLine(generator.generateString("using System;"));
