ccef445bdb375676b07f2b24aa5b9af2ce9b12e0
smdn <smdn@smdn.jp>
2022-12-15 23:50:08 +0900

    add example for Highlight.EnumerateSyntaxFilesWithDescription

---
+++ examples/list-syntaxes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Smdn.LibHighlightSharp;
+using Smdn.LibHighlightSharp.Bindings;
+
+const int columnWidthName = 16;
+const int columnWidthDescription = 48;
+
+Console.WriteLine($"|{("Name"), -columnWidthName}|{("Description"), -columnWidthDescription}|");
+Console.WriteLine($"|{new string('-', columnWidthName)}|{new string('-', columnWidthDescription)}|");
+
+using var hl = new Highlight();
+
+foreach (var syntax in hl.EnumerateSyntaxFilesWithDescription().OrderBy(static s => Path.GetFileName(s.Path))) {
+  Console.WriteLine($"|{Path.GetFileNameWithoutExtension(syntax.Path), -columnWidthName}|{syntax.Description, -columnWidthDescription}|");
+}
