ab289f5fef75dcfc800fb391236eb852abfb476b
smdn <smdn@smdn.jp>
2023-06-06 21:55:15 +0900

    suppress warning CA1032

--- src/Smdn.LibHighlightSharp/Smdn.LibHighlightSharp/HighlightThemeException.cs
+++ src/Smdn.LibHighlightSharp/Smdn.LibHighlightSharp/HighlightThemeException.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: GPL-3.0-or-later
+#pragma warning disable CA1032
+
 using System;
 
 namespace Smdn.LibHighlightSharp;
